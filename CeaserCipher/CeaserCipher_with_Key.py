d7385796b9828c44992bbe7c0608a31975277259
Aswinr24 <135364633+Aswinr24@users.noreply.github.com>
2024-02-09 21:18:56 +0530

    Create CeaserCipher_with_Key.py

---
+++ CeaserCipher/CeaserCipher_with_Key.py
@@ -0,0 +1,26 @@
+# The string to be encrypted/decrypted:
+message = input()
+# The encryption/decryption key:
+key = int(input("Enter the key:"))
+# Whether the program encrypts or decrypts:
+mode = 'encrypt' # Set to either 'encrypt' or 'decrypt'
+set = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+translated = ''
+for text in message:
+  if text in set:
+    textIndex = set.find(text)
+    print(textIndex)
+    if mode == 'encrypt':
+     translatedtext = textIndex + key
+     print(translatedtext)
+    elif mode == 'decrypt':
+     translatedtext = textIndex - key
+     
+    if translatedtext >= len(set):
+      translatedtext = translatedtext - len(set)
+    elif translatedtext < 0:
+      translatedtext = translatedtext + len(set)
+    translated = translated + set[translatedtext]
+  else:
+    translated = translated + text
+print(translated)
