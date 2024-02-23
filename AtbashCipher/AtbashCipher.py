b71a4058fff073378513941b8a5677310214dea9
Aswinr24 <135364633+Aswinr24@users.noreply.github.com>
2024-02-23 20:34:21 +0530

    Update AtbashCipher.py

--- AtbashCipher/AtbashCipher.py
+++ AtbashCipher/AtbashCipher.py
@@ -1,1 +1,11 @@
-#atbashcipher
+message=input()
+decrypted = ""
+    for text in message:
+        if text.isalpha():
+            if text.islower():
+                decrypted += chr(ord('z') - (ord(text) - ord('a')))
+            elif text.isupper():
+                decrypted += chr(ord('Z') - (ord(text) - ord('A')))
+        else:
+            decrypted += text
+    print(decrypted)
